47155d97b6136d70fc36568744c3912c74ed7c62
kiliter <kiliter@qq.com>
2025-05-07 11:48:10 +0800

    init

--- plugins.v2/libraryrescraper/__init__.py
+++ plugins.v2/libraryrescraper/__init__.py
@@ -1,64 +1,68 @@
-import threading
 from datetime import datetime, timedelta
 from pathlib import Path
-from threading import Event as ThreadEvent
+from threading import Event
 from typing import List, Tuple, Dict, Any
 
 import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 
+from app import schemas
+from app.chain.media import MediaChain
 from app.core.config import settings
-from app.core.event import eventmanager, Event
+from app.core.metainfo import MetaInfoPath
+from app.db.transferhistory_oper import TransferHistoryOper
+from app.helper.nfo import NfoReader
 from app.log import logger
 from app.plugins import _PluginBase
-from app.plugins.ffmpegthumb.ffmpeg_helper import FfmpegHelper
-from app.schemas import TransferInfo
-from app.schemas.types import EventType
+from app.schemas import MediaType
 from app.utils.system import SystemUtils
 
-ffmpeg_lock = threading.Lock()
 
-
-class FFmpegThumb(_PluginBase):
+class LibraryScraper(_PluginBase):
     # 插件名称
-    plugin_name = "FFmpeg缩略图"
+    plugin_name = "媒体库刮削改"
     # 插件描述
-    plugin_desc = "TheMovieDb没有背景图片时使用FFmpeg截取视频文件缩略图。"
+    plugin_desc = "定时对媒体库进行刮削，补齐缺失元数据和图片。"
     # 插件图标
-    plugin_icon = "ffmpeg.png"
+    plugin_icon = "scraper.png"
     # 插件版本
-    plugin_version = "2.1"
+    plugin_version = "0.0.1"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "kiliter"
     # 作者主页
-    author_url = "https://github.com/jxxghp"
+    author_url = "https://github.com/kiliter"
     # 插件配置项ID前缀
-    plugin_config_prefix = "ffmpegthumb_"
+    plugin_config_prefix = "libraryrescraper_"
     # 加载顺序
-    plugin_order = 31
+    plugin_order = 7
     # 可使用的用户级别
     user_level = 1
 
     # 私有属性
+    transferhis = None
+    mediachain = None
     _scheduler = None
+    _scraper = None
+    # 限速开关
     _enabled = False
     _onlyonce = False
     _cron = None
-    _timeline = "00:03:01"
-    _scan_paths = ""
+    _mode = ""
+    _scraper_paths = ""
     _exclude_paths = ""
     # 退出事件
-    _event = ThreadEvent()
+    _event = Event()
 
     def init_plugin(self, config: dict = None):
+        self.mediachain = MediaChain()
         # 读取配置
         if config:
             self._enabled = config.get("enabled")
             self._onlyonce = config.get("onlyonce")
             self._cron = config.get("cron")
-            self._timeline = config.get("timeline")
-            self._scan_paths = config.get("scan_paths") or ""
+            self._mode = config.get("mode") or ""
+            self._scraper_paths = config.get("scraper_paths") or ""
             self._exclude_paths = config.get("exclude_paths") or ""
 
         # 停止现有任务
@@ -66,35 +70,28 @@ class FFmpegThumb(_PluginBase):
 
         # 启动定时任务 & 立即运行一次
         if self._enabled or self._onlyonce:
-            self._scheduler = BackgroundScheduler(timezone=settings.TZ)
-            if self._cron:
-                logger.info(f"FFmpeg缩略图服务启动，周期：{self._cron}")
-                try:
-                    self._scheduler.add_job(func=self.__libraryscan,
-                                            trigger=CronTrigger.from_crontab(self._cron),
-                                            name="FFmpeg缩略图")
-                except Exception as e:
-                    logger.error(f"FFmpeg缩略图服务启动失败，原因：{str(e)}")
-                    self.systemmessage.put(f"FFmpeg缩略图服务启动失败，原因：{str(e)}", title="FFmpeg缩略图")
+            self.transferhis = TransferHistoryOper()
+
             if self._onlyonce:
-                logger.info(f"FFmpeg缩略图服务，立即运行一次")
-                self._scheduler.add_job(func=self.__libraryscan, trigger='date',
+                logger.info(f"媒体库刮削服务，立即运行一次")
+                self._scheduler = BackgroundScheduler(timezone=settings.TZ)
+                self._scheduler.add_job(func=self.__libraryscraper, trigger='date',
                                         run_date=datetime.now(tz=pytz.timezone(settings.TZ)) + timedelta(seconds=3),
-                                        name="FFmpeg缩略图")
+                                        name="媒体库刮削")
                 # 关闭一次性开关
                 self._onlyonce = False
                 self.update_config({
                     "onlyonce": False,
                     "enabled": self._enabled,
                     "cron": self._cron,
-                    "timeline": self._timeline,
-                    "scan_paths": self._scan_paths,
+                    "mode": self._mode,
+                    "scraper_paths": self._scraper_paths,
                     "exclude_paths": self._exclude_paths
                 })
-            if self._scheduler.get_jobs():
-                # 启动服务
-                self._scheduler.print_jobs()
-                self._scheduler.start()
+                if self._scheduler.get_jobs():
+                    # 启动服务
+                    self._scheduler.print_jobs()
+                    self._scheduler.start()
 
     def get_state(self) -> bool:
         return self._enabled
@@ -105,6 +102,35 @@ class FFmpegThumb(_PluginBase):
 
     def get_api(self) -> List[Dict[str, Any]]:
         pass
+
+    def get_service(self) -> List[Dict[str, Any]]:
+        """
+        注册插件公共服务
+        [{
+            "id": "服务ID",
+            "name": "服务名称",
+            "trigger": "触发器：cron/interval/date/CronTrigger.from_crontab()",
+            "func": self.xxx,
+            "kwargs": {} # 定时器参数
+        }]
+        """
+        if self._enabled and self._cron:
+            return [{
+                "id": "LibraryScraper",
+                "name": "媒体库刮削",
+                "trigger": CronTrigger.from_crontab(self._cron),
+                "func": self.__libraryscraper,
+                "kwargs": {}
+            }]
+        elif self._enabled:
+            return [{
+                "id": "LibraryScraper",
+                "name": "媒体库刮削",
+                "trigger": CronTrigger.from_crontab("0 0 */7 * *"),
+                "func": self.__libraryscraper,
+                "kwargs": {}
+            }]
+        return []
 
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         return [
@@ -159,11 +185,14 @@ class FFmpegThumb(_PluginBase):
                                 },
                                 'content': [
                                     {
-                                        'component': 'VTextField',
+                                        'component': 'VSelect',
                                         'props': {
-                                            'model': 'timeline',
-                                            'label': '截取时间',
-                                            'placeholder': '00:03:01'
+                                            'model': 'mode',
+                                            'label': '覆盖模式',
+                                            'items': [
+                                                {'title': '不覆盖已有元数据', 'value': ''},
+                                                {'title': '覆盖所有元数据和图片', 'value': 'force_all'},
+                                            ]
                                         }
                                     }
                                 ]
@@ -179,8 +208,8 @@ class FFmpegThumb(_PluginBase):
                                         'component': 'VCronField',
                                         'props': {
                                             'model': 'cron',
-                                            'label': '定时扫描周期',
-                                            'placeholder': '5位cron表达式，留空关闭'
+                                            'label': '执行周期',
+                                            'placeholder': '5位cron表达式，留空自动'
                                         }
                                     }
                                 ]
@@ -199,8 +228,8 @@ class FFmpegThumb(_PluginBase):
                                     {
                                         'component': 'VTextarea',
                                         'props': {
-                                            'model': 'scan_paths',
-                                            'label': '定时扫描路径',
+                                            'model': 'scraper_paths',
+                                            'label': '削刮路径',
                                             'rows': 5,
                                             'placeholder': '每一行一个目录'
                                         }
@@ -222,7 +251,7 @@ class FFmpegThumb(_PluginBase):
                                         'component': 'VTextarea',
                                         'props': {
                                             'model': 'exclude_paths',
-                                            'label': '定时扫描排除路径',
+                                            'label': '排除路径',
                                             'rows': 2,
                                             'placeholder': '每一行一个目录'
                                         }
@@ -245,7 +274,8 @@ class FFmpegThumb(_PluginBase):
                                         'props': {
                                             'type': 'info',
                                             'variant': 'tonal',
-                                            'text': '开启插件后默认会实时处理增量整理的媒体文件，需要处理存量媒体文件时才需开启定时；需要提前安装FFmpeg：https://www.ffmpeg.org'
+                                            'text': '刮削路径后拼接#电视剧/电影，强制指定该媒体路径媒体类型。'
+                                                    '不加默认根据文件名自动识别媒体类型。'
                                         }
                                     }
                                 ]
@@ -256,63 +286,49 @@ class FFmpegThumb(_PluginBase):
             }
         ], {
             "enabled": False,
-            "cron": "",
-            "timeline": "00:03:01",
-            "scan_paths": "",
+            "cron": "0 0 */7 * *",
+            "mode": "",
+            "scraper_paths": "",
             "err_hosts": ""
         }
 
     def get_page(self) -> List[dict]:
         pass
 
-    @eventmanager.register(EventType.TransferComplete)
-    def scan_rt(self, event: Event):
+    def __libraryscraper(self):
         """
-        根据事件实时扫描缩略图
+        开始刮削媒体库
         """
-        if not self._enabled:
-            return
-        # 事件数据
-        transferinfo: TransferInfo = event.event_data.get("transferinfo")
-        if not transferinfo:
-            return
-        if transferinfo.target_diritem and transferinfo.target_diritem.storage != "local":
-            logger.warn(f"FFmpeg缩略图不支持非本地存储：{transferinfo.target_diritem.storage}")
-            return
-        file_list = transferinfo.file_list_new
-        for file in file_list:
-            logger.info(f"FFmpeg缩略图处理文件：{file}")
-            file_path = Path(file)
-            if not file_path.exists():
-                logger.warn(f"{file_path} 不存在")
-                continue
-            if file_path.suffix not in settings.RMT_MEDIAEXT:
-                logger.warn(f"{file_path} 不是支持的视频文件")
-                continue
-            self.gen_file_thumb(file_path)
-
-    def __libraryscan(self):
-        """
-        开始扫描媒体库
-        """
-        if not self._scan_paths:
+        if not self._scraper_paths:
             return
         # 排除目录
         exclude_paths = self._exclude_paths.split("\n")
         # 已选择的目录
-        paths = self._scan_paths.split("\n")
+        paths = self._scraper_paths.split("\n")
+        # 需要适削的媒体文件夹
+        scraper_paths = []
         for path in paths:
             if not path:
                 continue
-            scan_path = Path(path)
-            if not scan_path.exists():
-                logger.warning(f"FFmpeg缩略图扫描路径不存在：{path}")
+            # 强制指定该路径媒体类型
+            mtype = None
+            if str(path).count("#") == 1:
+                mtype = next(
+                    (mediaType for mediaType in MediaType.__members__.values() if
+                     mediaType.value == str(str(path).split("#")[1])),
+                    None)
+                path = str(path).split("#")[0]
+            # 判断路径是否存在
+            scraper_path = Path(path)
+            if not scraper_path.exists():
+                logger.warning(f"媒体库刮削路径不存在：{path}")
                 continue
-            logger.info(f"开始FFmpeg缩略图扫描：{path} ...")
-            # 遍历目录下的所有文件
-            for file_path in SystemUtils.list_files(scan_path, extensions=settings.RMT_MEDIAEXT):
+            logger.info(f"开始检索目录：{path} {mtype} ...")
+            # 遍历所有文件
+            files = SystemUtils.list_files(scraper_path, settings.RMT_MEDIAEXT)
+            for file_path in files:
                 if self._event.is_set():
-                    logger.info(f"FFmpeg缩略图扫描服务停止")
+                    logger.info(f"媒体库刮削服务停止")
                     return
                 # 排除目录
                 exclude_flag = False
@@ -326,26 +342,110 @@ class FFmpegThumb(_PluginBase):
                 if exclude_flag:
                     logger.debug(f"{file_path} 在排除目录中，跳过 ...")
                     continue
-                # 开始处理文件
-                self.gen_file_thumb(file_path)
-            logger.info(f"目录 {path} 扫描完成")
+                # 识别是电影还是电视剧
+                if not mtype:
+                    file_meta = MetaInfoPath(file_path)
+                    mtype = file_meta.type
+                # 重命名格式
+                rename_format = settings.TV_RENAME_FORMAT \
+                    if mtype == MediaType.TV else settings.MOVIE_RENAME_FORMAT
+                # 计算重命名中的文件夹层数
+                rename_format_level = len(rename_format.split("/")) - 1
+                if rename_format_level < 1:
+                    continue
+                # 取相对路径的第1层目录
+                media_path = file_path.parents[rename_format_level - 1]
+                dir_item = (media_path, mtype)
+                if dir_item not in scraper_paths:
+                    logger.info(f"发现目录：{dir_item}")
+                    scraper_paths.append(dir_item)
+        # 开始刮削
+        if scraper_paths:
+            for item in scraper_paths:
+                logger.info(f"开始刮削目录：{item[0]} ...")
+                self.__scrape_dir(path=item[0], mtype=item[1])
+        else:
+            logger.info(f"未发现需要刮削的目录")
 
-    def gen_file_thumb(self, file_path: Path):
+    def __scrape_dir(self, path: Path, mtype: MediaType):
         """
-        处理一个文件
+        削刮一个目录，该目录必须是媒体文件目录
         """
-        # 单线程处理
-        with ffmpeg_lock:
-            try:
-                thumb_path = file_path.with_name(file_path.stem + "-thumb.jpg")
-                if thumb_path.exists():
-                    logger.info(f"缩略图已存在：{thumb_path}")
-                    return
-                if FfmpegHelper.get_thumb(video_path=str(file_path),
-                                          image_path=str(thumb_path), frames=self._timeline):
-                    logger.info(f"{file_path} 缩略图已生成：{thumb_path}")
-            except Exception as err:
-                logger.error(f"FFmpeg处理文件 {file_path} 时发生错误：{str(err)}")
+        # 优先读取本地nfo文件
+        tmdbid = None
+        if mtype == MediaType.MOVIE:
+            # 电影
+            movie_nfo = path / "movie.nfo"
+            if movie_nfo.exists():
+                tmdbid = self.__get_tmdbid_from_nfo(movie_nfo)
+            file_nfo = path / (path.stem + ".nfo")
+            if not tmdbid and file_nfo.exists():
+                tmdbid = self.__get_tmdbid_from_nfo(file_nfo)
+        else:
+            # 电视剧
+            tv_nfo = path / "tvshow.nfo"
+            if tv_nfo.exists():
+                tmdbid = self.__get_tmdbid_from_nfo(tv_nfo)
+        if tmdbid:
+            # 按TMDBID识别
+            logger.info(f"读取到本地nfo文件的tmdbid：{tmdbid}")
+            mediainfo = self.chain.recognize_media(tmdbid=tmdbid, mtype=mtype)
+        else:
+            # 按名称识别
+            meta = MetaInfoPath(path)
+            meta.type = mtype
+            mediainfo = self.chain.recognize_media(meta=meta)
+        if not mediainfo:
+            logger.warn(f"未识别到媒体信息：{path}")
+            return
+
+        # 如果未开启新增已入库媒体是否跟随TMDB信息变化则根据tmdbid查询之前的title
+        if not settings.SCRAP_FOLLOW_TMDB:
+            transfer_history = self.transferhis.get_by_type_tmdbid(tmdbid=mediainfo.tmdb_id,
+                                                                   mtype=mediainfo.type.value)
+            if transfer_history:
+                mediainfo.title = transfer_history.title
+        # 获取图片
+        self.chain.obtain_images(mediainfo)
+        # 刮削
+        self.mediachain.scrape_metadata(
+            fileitem=schemas.FileItem(
+                storage="local",
+                type="dir",
+                path=str(path).replace("\\", "/") + "/",
+                name=path.name,
+                basename=path.stem,
+                modify_time=path.stat().st_mtime,
+            ),
+            mediainfo=mediainfo,
+            overwrite=True if self._mode else False
+        )
+        logger.info(f"{path} 刮削完成")
+
+    @staticmethod
+    def __get_tmdbid_from_nfo(file_path: Path):
+        """
+        从nfo文件中获取信息
+        :param file_path:
+        :return: tmdbid
+        """
+        if not file_path:
+            return None
+        xpaths = [
+            "uniqueid[@type='Tmdb']",
+            "uniqueid[@type='tmdb']",
+            "uniqueid[@type='TMDB']",
+            "tmdbid"
+        ]
+        try:
+            reader = NfoReader(file_path)
+            for xpath in xpaths:
+                tmdbid = reader.get_element_value(xpath)
+                if tmdbid:
+                    return tmdbid
+        except Exception as err:
+            logger.warn(f"从nfo文件中获取tmdbid失败：{str(err)}")
+        return None
 
     def stop_service(self):
         """
